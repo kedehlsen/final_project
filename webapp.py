68f10c4d482b509b7ef34530e3b5f13050d96ba8
kedehlsen <54326372+kedehlsen@users.noreply.github.com>
2020-05-25 22:06:24 -0700

    Added flask import lines, debug line and main line

--- webapp.py
+++ webapp.py
@@ -1,1 +1,15 @@
+from flask import Flask, redirect, url_for, session, request, jsonify
+from flask_oauthlib.client import OAuth
+from flask import render_template
+from flask import flash
 
+import pprint
+import os
+
+app = Flask(__name__)
+
+app.debug = True #Change this to False for production
+
+
+if __name__ == '__main__':
+    app.run()
